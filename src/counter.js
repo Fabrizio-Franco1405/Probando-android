1de13324210df09b8ffd9b4d5351302f4011e17a
Mcell <mcell.mercadolibre@gmail.com>
2025-08-26 13:13:09 -0400

    Update counter.js

--- src/counter.js
+++ src/counter.js
@@ -2,7 +2,11 @@ export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
-    element.innerHTML = `count is ${counter}`
+    if (counter == 1) {
+      element.innerHTML = `Presionamela ${counter} vez`
+    } else {
+      element.innerHTML = `Presionamela ${counter} veces`
+    }
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
